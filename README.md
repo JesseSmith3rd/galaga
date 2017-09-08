af28fe534d1ebc62f715e66f624d8e4c4732ebad
Shaun <seyyes2000@hotmail.com>
2017-09-08 11:14:29 -0400

    finish game

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# galaga-python-game
+# Galaga Python Game made with Pygame
+
+## Run Instructions
+1. Download files
+2. Open terminal
+3. cd to game directory
+4. `$ python3 main.py`
+
+## Play Instructions
+1. Move mouse or cursor to move player
+2. Click for fire lasers
--- main.py
+++ main.py
@@ -1,27 +1,79 @@
 # Source: https://github.com/pathunstrom/game-tutorial
-
+import csv
+from itertools import count
 from os import path
 
 from ppb import GameEngine, BaseScene
-from pygame import image, mouse
+from pygame import image, mouse, Rect
 from pygame.sprite import DirtySprite, groupcollide
 
 
-class Player(DirtySprite):
+
+ROOT_DIR = path.dirname(__file__)
+
+
+class Spawner(object):
+    def __init__(self, scene, generator, enemy_class):
+        self.scene = scene
+        self.enemy_class = enemy_class
+        self.generator = generator
+        self.time = 0
+        self.next_time = None
+        self.next_position = None
+        self.running = True
+        self.prime()
+
+    def spawn(self, time_delta):
+        self.time += time_delta
+        while self.running and self.time >= self.next_time:
+            self.enemy_class(self.scene, self.next_position)
+            self.prime()
+
+    def prime(self):
+        try:
+            self.next_time, self.next_position = next(self.generator)
+        except StopIteration:
+            self.running = False
+
+
+class BaseGameObject(DirtySprite):
+    group = None
+    image_path = None
+    image = None
+
+    def __init__(self, scene, position=(0, 0)):
+        cls = self.__class__
+        cls.group = cls.__name__
+        super().__init__(scene.groups[cls.group], scene.groups["render"])
+        self.scene = scene
+        cls.image = image.load(path.join(ROOT_DIR, cls.image_path))
+        self.rect = cls.image.get_rect()
+        self.rect.center = position
+        # self.rect.center = (720, 800)
+        self.last_position = position
+
+    def update(self, time_delta):
+        self.simulate(time_delta)
+        if self.rect.center != self.last_position:
+            self.dirty = True
+            self.last_position = self.rect.center
+    # def simulate(self, time_delta):
+    #     pass
+
+
+class Player(BaseGameObject):
+    image_path = "player.png"
 
     def __init__(self, scene):
-        super().__init__(scene.groups["player"])
-        self.image = image.load(path.join(path.dirname(__file__),
-                                          "player.png"))
-        self.rect = self.image.get_rect()
-        self.scene = scene
-        self.laser_limiter = 0.25
+        super().__init__(scene)
+        # self.rect.center = (720, 0)
+        self.laser_limiter = 0.5
         self.laser_delay = 0
 
     def simulate(self, time_delta):
         mouse_x, mouse_y = mouse.get_pos()
-        diff_x = max(min(mouse_x - self.rect.centerx, 5), -5)
-        diff_y = max(min(mouse_y - self.rect.centery, 5), -5)
+        diff_x = max(min(mouse_x - self.rect.centerx, 10), -10)
+        diff_y = max(min(mouse_y - self.rect.centery, 10), -10)
         self.rect.centerx += diff_x
         self.rect.centery += diff_y
 
@@ -32,19 +84,21 @@ class Player(DirtySprite):
         self.laser_delay += time_delta
 
 
-class Laser(DirtySprite):
+class Laser(BaseGameObject):
+    image_path = "laser.png"
 
-    def __init__(self, scene, position):
-        super().__init__(scene.groups["lasers"])
-        b_image = image.load(path.join(path.dirname(__file__), "laser.png"))
-        self.image = b_image
-        self.rect = self.image.get_rect()
-        self.rect.midbottom = position
-        self.scene = scene
-
-    def update(self, time_delta):
+    def simulate(self, time_delta):
         self.rect.centery += -10
-        self.dirty = True
+
+
+class Enemy(BaseGameObject):
+    image_path = "enemy.png"
+
+    def __init__(self, scene, x_position):
+        super().__init__(scene, (x_position, 50))
+
+    def simulate(self, time_delta):
+        self.rect.centery += 3
 
 
 class Game(BaseScene):
@@ -55,38 +109,40 @@ class Game(BaseScene):
                          **kwargs)
         engine.display.fill(background_color)
         Player(self)
-        Laser(self, (300, 400)).kill()
-        Enemy(self, 200)
+        Laser(self, (0, 0)).kill()
+        self.spawner = Spawner(self, file_spawner('spawn.csv'), Enemy)
+        # Enemy(self, 200)
 
     def simulate(self, time_delta):
         super().simulate(time_delta)
-        player = self.groups["player"]
-        lasers = self.groups["lasers"]
-        enemies = self.groups["enemy"]
+        self.spawner.spawn(time_delta)
+        player = self.groups[Player.group]
+        lasers = self.groups[Laser.group]
+        enemies = self.groups[Enemy.group]
         groupcollide(player, enemies, True, True)
         groupcollide(enemies, lasers, True, True)
 
 
-class Enemy(DirtySprite):
+# Generator that outputs 2-tuple timestamp in secs
+def simple_infinite_spawn(time_step, x_val):
+    for value in count(1):
+        time = time_step * value
+        yield time, x_val
 
-    def __init__(self, scene, x_position):
-        super().__init__(scene.groups["enemy"])
-        p_image = image.load(path.join(path.dirname(__file__), "enemy.png"))
-        self.image = p_image
-        self.rect = self.image.get_rect()
-        self.rect.bottom = 0
-        self.rect.centerx = x_position
-        self.scene = scene
 
-    def update(self, time_delta):
-        self.rect.centery += 3
-        self.dirty = True
+# Create new generator
+def file_spawner(file_name):
+    with open(path.join(path.dirname(__file__), file_name), "r") as spawn_file:
+        spawn_reader = csv.reader(spawn_file)
+        for row in spawn_reader:
+            yield float(row[0]), int(row[1])
 
 
 # Main loop to keep the GUI running (60 times/sec)
 def main():
-    with GameEngine(Game, resolution=(500, 750)) as engine:
+    with GameEngine(Game, resolution=(1440, 900)) as engine:
         engine.run()
 
 if __name__ == "__main__":
     main()
+
